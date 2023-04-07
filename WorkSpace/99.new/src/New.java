69baad5142424ec875bf2d4335b2b8f309a068eb
joeunpyeong <128439456+joeunpyeong@users.noreply.github.com>
2023-04-07 14:17:23 +0900

    00

--- WorkSpace/99.new/src/New.java
+++ WorkSpace/99.new/src/New.java
@@ -4,7 +4,10 @@ public class New {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); //스캐너 준비
 		
-		int power, agi, inte, nar, app, hp=1, money, st, st1;
+		int st=0, st1=0; //스토리
+		int power, agi, inte, nar, app, hp=1, money, fame=0; //능력치
+		int cat=0; //장비,동물,도구
+		String catName = null;
 		//시작
 		//if, hp가 0이면 break 을 해서 while(reStart==1)로 가도록
 		// 1이상이면 continue(굳이 안써도 됨)
@@ -39,7 +42,11 @@ public class New {
 		
 				System.out.print("체력 : ");
 				hp = (int)(Math.random()*10)+1;
-				System.out.println(app);
+				System.out.println(hp);
+				
+				System.out.print("명성 : ");
+				fame = (int)(Math.random()*10)+1;
+				System.out.println(fame);
 
 				System.out.print("보유한 금액 : ");
 				money = ((int)(Math.random()*10)+1)*100;
@@ -50,12 +57,12 @@ public class New {
 				//1
 				System.out.println("찬 바람에 눈을 뜬 당신은 울창한 숲 속에서 나뭇잎을 헤치며 깨어났습니다.\n분명 집 안에서 깨어났어야하는데 말이죠.\n");
 				System.out.println("1.여기저기 돌아다녀 본다. 2.나무 위에 올라가서 주위를 살핀다.");
-		
-				System.out.print("= >");
+			while(true) {
+				System.out.print("=>");
 				st = Integer.parseInt(sc.nextLine());
 				if(st==1) { //1-1
-					System.out.println("울창한 숲 속을 돌아다니던 중 사나운 고양이(힘:2 ,민첩 3)와 마주쳤습니다. 싸우시겠습니까?\n 1.싸운다 2.도망친다");
-					System.out.print("= >");
+					System.out.println("울창한 숲 속을 돌아다니던 중 사나운 고양이(힘:2 ,민첩 3)와 마주쳤습니다. 싸우시겠습니까?\n 1.싸운다(힘:2이상) 2.도망친다(민첩:3이상) 3.길들인다(지능:6이상)");
+					System.out.print("=>");
 					st1 = Integer.parseInt(sc.nextLine());
 					if(st1==1) {//1-1-1
 						if(agi>=4) {//1-1-1.1
@@ -67,6 +74,7 @@ public class New {
 							System.out.println("(체력이 2 감소했습니다. 현제 체력 :"+hp+")");
 							System.out.print("당신은 지친 몸을 이끌며 걷다보니 ");
 						}
+						break;
 					}else if(st1==2) { //1-1-2
 						if(agi>=4) {//1-1-2.1
 							System.out.println("당신은 무사히 도망쳤습니다.");
@@ -77,20 +85,48 @@ public class New {
 							System.out.println("(체력이 2 감소했습니다. 현제 체력 :"+hp+")");
 							System.out.print("당신은 지친 몸을 이끌며 걷다보니 ");
 						}
+						break;
+					}else if(st1==3) {
+						System.out.print("고양이에게 다가가 길들이기를 시도합니다.");
+						if(inte>=6) {//1-1-2.1
+							System.out.println("오래전 TV에서 본 지식을 통해 고양이를 길들일 수 있었지만 길들이는 과정에서 고양이에게 긁혔습니다.");
+							hp--;
+							System.out.println("(체력이 1 감소했습니다. 현제 체력 :"+hp+")");
+							cat++;
+							System.out.print("우여곡절 끝에 당신은 고양이를 길들였습니다. 앞으로 당신과 함께 있을 고양이의 이름을 지어주세요.\n=>");
+							catName = sc.nextLine();
+							System.out.println("당신은 고양이와 같이 길을 나서기 시작하자 ");
+						}else {//1-1-2.2
+							System.out.println("서툰 손길에 고양이가 흥분하며 당신을 할큅니다.");
+							hp-=6;
+							System.out.println("(체력이 6 감소했습니다. 현제 체력 :"+hp+")");
+							System.out.print("당신은 지친 몸을 이끌며 걷다보니 ");
+						}
+						System.out.println("작은 마을을 발견했습니다.");
+						break;
 					}
-					System.out.println("작은 마을을 발견했습니다.");
-				}else if(st==2){//1-2
-					System.out.println("나무 위에서 내려다보니 몇 미터 안되는 곳에 작은 마을이 보입니다.");
+					}else if(st==2){//1-2
+						System.out.println("나무 위에서 내려다보니 몇 미터 안되는 곳에 작은 마을이 보입니다.");
+						break;
+					}else {
+						System.out.println("존재하지 않는 선택지입니다. 다시 입력해 주세요.");
+					}
 				}
-			
 				//2
-				System.out.print("\n당신은 마을에 도착했습니다. 마을을 돌아다니려던 찰나\n두건으로 얼굴을 가린 사람이 당신의 이름을 묻습니다."
-								+ "알려주고 싶은 이름을 입력하세요. \n 내 이름은");	
+				System.out.print("\n당신은 마을 입구에 도착했습니다. 마을로 들어가려던 찰나\n피리를 든 사람이 당신에게 걸어오며 말을 겁니다.\n\"외지인이여 넌 누구지?\""
+								+ "알려주고 싶은 이름을 입력하세요. \n 내 이름은  ");	
 				String sSay = sc.nextLine();
-				if(userName == sSay) {//2-1
-					System.out.println("");  
-				}else {  //2-2
-					System.out.println("");  
+				if(!userName .equals(sSay)) {  //   .equals()    ->문자열 비교 
+					System.out.println("낯선 사람이 얼굴을 찌뿌리며 창을 들이댑니다. 그리고 서서히 입을 열기 시작했습니다. \"넌 지금 거짓말을 하고 있군\"");  
+					fame--;
+					System.out.println("(명성이 1 감소했습니다. 현제 체력 :"+fame+")");
+				}
+				System.out.print("\"너의 이름을 물어본게 아니다.");
+				if(cat>1) {
+					System.out.println("어찌 됐ㄷ..\"/n"+ "낯선 사람은 "+catName+"");
+					
+				}else {
+					System.out.println("어찌 됬든.. 이 숲에서 당장 나가라. 경고는 여기까지다 한번 더 눈에 보인다면...\"");
 				}
 		
 			
